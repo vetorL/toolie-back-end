bc4eb847560b43f66cffa9627b47ce9629924fd2
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-01 20:27:39 -0300

    Adiciona campo "nome" em Usuario

--- src/main/java/com/toolie/back_end/usuario/Usuario.java
+++ src/main/java/com/toolie/back_end/usuario/Usuario.java
@@ -1,6 +1,9 @@
 package com.toolie.back_end.usuario;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -14,5 +17,10 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, max = 255, message = "Nome deve ter entre 2 e 255 caracteres")
+    @Pattern(regexp = "[\\p{IsLatin}\\p{Digit}]*", message = "Nome deve possuir apenas caracteres alfanuméricos")
+    private final String nome;
 
 }
