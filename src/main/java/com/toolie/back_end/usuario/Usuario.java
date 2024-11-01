0ea97a3b0103b240025c9a58511c6f7b82cf4a03
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-01 20:27:06 -0300

    Adiciona campo id ao Usuario

--- src/main/java/com/toolie/back_end/usuario/Usuario.java
+++ src/main/java/com/toolie/back_end/usuario/Usuario.java
@@ -1,5 +1,18 @@
 package com.toolie.back_end.usuario;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
 }
