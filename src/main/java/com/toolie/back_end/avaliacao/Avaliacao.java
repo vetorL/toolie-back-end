550de62af2b70916a63f268ec299efd74e333610
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-02 18:12:47 -0300

    Adiciona campos da entidade Avaliacao
    Validacao ainda precisa ser implementada

--- src/main/java/com/toolie/back_end/avaliacao/Avaliacao.java
+++ src/main/java/com/toolie/back_end/avaliacao/Avaliacao.java
@@ -1,5 +1,31 @@
 package com.toolie.back_end.avaliacao;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Data
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class Avaliacao {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private final Integer nota;
+
+    private final String comentario;
+
+    @CreationTimestamp
+    private Date data_avaliacao;
+
 }
