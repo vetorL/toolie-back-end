6022257b3ff8f96b69f59a11773cb1a31d228cd9
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-02 18:45:42 -0300

    Adiciona endpoint /usuarios

---
+++ src/main/java/com/toolie/back_end/usuario/UsuarioController.java
@@ -0,0 +1,22 @@
+package com.toolie.back_end.usuario;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @GetMapping
+    public Iterable<Usuario> getAll() {
+        return usuarioRepository.findAll();
+    }
+
+}
