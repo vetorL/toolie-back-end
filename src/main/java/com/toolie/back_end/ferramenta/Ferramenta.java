0e298d52ad91f3b849a425958b1d3f4526587321
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-02 21:19:34 -0300

    Adiciona campos em Ferramenta

--- src/main/java/com/toolie/back_end/ferramenta/Ferramenta.java
+++ src/main/java/com/toolie/back_end/ferramenta/Ferramenta.java
@@ -1,4 +1,34 @@
 package com.toolie.back_end.ferramenta;
 
+import com.toolie.back_end.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class Ferramenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "proprietario_id")
+    private Usuario proprietario;
+
+    private final String tipoFerramenta;
+
+    // Estado de uso
+    private final String estado;
+
+    private final String descricao;
+
+    private final String disponibilidade;
+
+    private final String localizacao;
+
 }
