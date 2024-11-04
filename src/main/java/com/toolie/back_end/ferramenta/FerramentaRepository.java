0c2ca6fa259087331bf04aad452f23933aa8cbac
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-04 15:51:10 -0300

    Adiciona repositório para a entidade Ferramenta

---
+++ src/main/java/com/toolie/back_end/ferramenta/FerramentaRepository.java
@@ -0,0 +1,6 @@
+package com.toolie.back_end.ferramenta;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FerramentaRepository extends CrudRepository<Ferramenta, Long> {
+}
