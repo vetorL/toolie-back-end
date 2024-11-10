a10fe1b1f5acd3de779d942763dcfb8c6fef81d5
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-10 02:39:56 -0300

    Adiciona metodo findByLocatarioId

--- src/main/java/com/toolie/back_end/aluguel/AluguelRepository.java
+++ src/main/java/com/toolie/back_end/aluguel/AluguelRepository.java
@@ -2,5 +2,10 @@ package com.toolie.back_end.aluguel;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AluguelRepository extends CrudRepository<Aluguel, Long> {
+
+    List<Aluguel> findByLocatarioId(long locatarioId);
+
 }
